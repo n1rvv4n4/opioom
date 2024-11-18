c562455de59d40cdfde838c02b65cfc8bf574bd9
n1rvv4n4 <gogol.vasilich@gmail.com>
2024-11-18 15:50:54 +0700

    Создание структуры и добавление файлов для новых страниц

--- server.js
+++ server.js
@@ -3,12 +3,8 @@ const cors = require('cors');
 const requestIp = require('request-ip');
 const fs = require('fs');
 const path = require('path');
-
-// Импорт логгера
 const logger = require('./utils/logger');
-
-// Подключение к базе данных
-const sequelize = require('./db'); // путь к вашему файлу конфигурации sequelize
+const sequelize = require('./db');
 
 // Создаем Express приложение
 const app = express();
@@ -18,19 +14,45 @@ app.use(cors());
 app.use(express.json());
 app.use(requestIp.mw());
 
-// Импорт и использование маршрутов
+// Папка со статическими файлами
+const staticPath = path.join(__dirname, 'site');
+app.use(express.static(staticPath));
+
+// Импорт и использование маршрутов для API
 const sessionRoutes = require('./routes/sessionRoutes');
 app.use('/api/sessions', sessionRoutes);
+
+// Маршрут для главной страницы
+app.get('/', (req, res) => {
+    res.sendFile(path.join(staticPath, 'mainpage.html'));
+});
+
+// Маршрут для страницы выбора опроса
+app.get('/propala', (req, res) => {
+    res.sendFile(path.join(staticPath, 'propala.html'));
+});
+
+// Динамические маршруты для опросов
+app.get('/propala/kto_ty_is_:survey', (req, res) => {
+    const surveyName = req.params.survey;
+    const surveyFilePath = path.join(staticPath, `${surveyName}.html`);
+
+    // Проверка, существует ли файл
+    if (fs.existsSync(surveyFilePath)) {
+        res.sendFile(surveyFilePath);
+    } else {
+        res.status(404).send('Опрос не найден');
+    }
+});
 
 // Обработка необработанных ошибок
 app.use((err, req, res, next) => {
     logger.error(err.message, err);
-    res.status(500).send('Something failed.');
+    res.status(500).send('Произошла ошибка.');
 });
 
 // Синхронизация базы данных и запуск сервера
-
-const all_ip = '0.0.0.0'
+const all_ip = '0.0.0.0';
 const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
 const serverIP = config.serverIP;
 const ServerPort = config.ServerPort;
